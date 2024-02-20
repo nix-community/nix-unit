1fda41fe253ec495a2884f50e1b2ab836a7a94b4
adisbladis <adisbladis@gmail.com>
2024-02-20 15:06:58 +1300

    Improve coverage doc example

--- lib/coverage.nix
+++ lib/coverage.nix
@@ -11,22 +11,39 @@ in
     Generate coverage testing for public interfaces.
 
     Example:
+
+    # Expression
     let
-    # The public interface (attrset) we are testing
-    public = {
-      addOne = x: x + 1;
-    };
-    # Test suite
-    tests = {
-      addOne = {
-        testAdd = {
-          expr = public.addOne 1;
-          expected = 2;
+      # The public interface (attrset) we are testing
+      public = {
+        addOne = x: x + 1;
+      };
+      # Test suite
+      tests = {
+        addOne = {
+          testAdd = {
+            expr = public.addOne 1;
+            expected = 2;
+          };
         };
       };
-    };
     in addCoverage public tests
 
+    # Returns
+    {
+      addOne = {
+        testAdd = {
+          expected = 2;
+          expr = 2;
+        };
+      };
+      coverage = {
+        testAddOne = {
+          expected = true;
+          expr = true;
+        };
+      };
+    }
   */
   addCoverage =
     # The public interface to generate coverage for
