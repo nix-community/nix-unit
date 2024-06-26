64882adb76c1ead29b802addefe1d6491e3f89cd
Jörg Thalheim <joerg@thalheim.io>
2024-06-26 11:45:06 +0200

    bump 2.23.0

--- default.nix
+++ default.nix
@@ -19,7 +19,7 @@ let
 in
 stdenv.mkDerivation {
   pname = "nix-unit";
-  version = "2.18.0";
+  version = "2.23.0";
   src = if srcDir == null then filterMesonBuild ./. else srcDir;
   buildInputs = [
     nlohmann_json
