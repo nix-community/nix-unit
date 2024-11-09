87925cfbc66598edac9fb23a8e96e083d6710ce3
Jörg Thalheim <joerg@thalheim.io>
2024-11-09 07:37:41 +0100

    treefmt: switch to nixfmt/ruff

--- dev/treefmt.nix
+++ dev/treefmt.nix
@@ -1,42 +1,11 @@
-{ pkgs, lib, ... }: {
+{ ... }:
+{
   # Used to find the project root
   projectRootFile = "flake.lock";
 
   programs.clang-format.enable = true;
-
-  settings.formatter = {
-    nix = {
-      command = "sh";
-      options = [
-        "-eucx"
-        ''
-          ${pkgs.lib.getExe pkgs.deadnix} --edit "$@"
-
-          for i in "$@"; do
-            ${pkgs.lib.getExe pkgs.statix} fix "$i"
-          done
-
-          ${pkgs.lib.getExe pkgs.nixpkgs-fmt} "$@"
-        ''
-        "--"
-      ];
-      includes = [ "*.nix" ];
-      excludes = [ ];
-    };
-
-    clang-format = { };
-
-    python = {
-      command = "sh";
-      options = [
-        "-eucx"
-        ''
-          ${pkgs.lib.getExe pkgs.python3.pkgs.black} "$@"
-          ${pkgs.lib.getExe pkgs.ruff} --fix "$@"
-        ''
-        "--" # this argument is ignored by bash
-      ];
-      includes = [ "*.py" ];
-    };
-  };
+  programs.deadnix.enable = true;
+  programs.nixfmt.enable = true;
+  programs.ruff.format = true;
+  programs.ruff.check = true;
 }
