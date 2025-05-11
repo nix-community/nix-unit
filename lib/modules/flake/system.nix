0457715c3116253eebc08fb1909b2ad35d0e24dc
Robert Hensing <robert@roberthensing.nl>
2025-05-11 15:17:30 +0200

    f

--- lib/modules/flake/system.nix
+++ lib/modules/flake/system.nix
@@ -111,17 +111,20 @@ in
               key = "";
             }
             ''
-              unset NIX_BUILD_TOP
               export HOME="$(realpath .)"
+              export NIX_REMOTE="$HOME/storedata"
+              echo out=$out
+              # why the heck not
+              ls /build/home/.local/share/nix/root/nix/store/
+              env | grep NIX_
               echo "Running tests for " ${lib.escapeShellArg system}
-              nix-unit --eval-store "$HOME" \
+              nix-unit \
                 --show-trace \
                 --extra-experimental-features flakes \
                 ${lib.concatStringsSep "\\\n  " (lib.mapAttrsToList overrideArg config.nix-unit.inputs)} \
                 --flake ${self}#tests.systems.${system} \
                 ;
-              env | grep NIX_
-              echo "Writing $key to $out"
+              echo "Writing \"$key\" to $out"
               echo -n "$key" > $out
             ''
         );
